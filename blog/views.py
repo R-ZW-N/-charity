16762502385edb6977f97db924f7bf2f40104ab7
RISVN <rizwanshaz2004@gmail.com>
2024-11-29 17:16:16 +0530

    second commit

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(requsest):
-    return HttpResponse('<h1>home_page</h1>')
+    return HttpResponse('<h1>home__page</h1>')
 
 def about(requsest):
-    return HttpResponse('<h1>about_page</h1>')
+    return HttpResponse('<h1>about__page</h1>')
 
